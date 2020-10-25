9a8ba9239fdbacc2988da3840a41e6cc485c7f25
Joao Veloso <joao.veloso@auroraer.com>
2020-10-25 14:27:19 +0000

    reformating api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const path = require('path');
 
@@ -7,6 +8,30 @@ const app = express();
 app.get('/', (reqeust, response) => {
     response.sendFile(path.join(__dirname,'public','index.html'));
 }) */
+
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
+
+app.get('/api/members', (request, response) => response.json(members));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
